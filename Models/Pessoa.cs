7d888f5713d9f82a2bd052fbd878f515eec73a21
Jean Carlos <jcpds14@gmail.com>
2022-09-22 05:16:43 -0300

    add validation to _nome

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,7 +7,25 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }//get obtem valor, set atribui valor
+        private string _nome;
+        public string Nome 
+        //get obtem valor, set atribui valor
+        { 
+            get
+            {
+                return _nome.ToUpper();
+            } 
+            
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+            
+        }
         public int Idade { get; set; }
 
         public void Apresentar()
