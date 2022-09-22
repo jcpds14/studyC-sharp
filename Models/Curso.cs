a9e7774f940e54f101ee025a09d93328ef5f09aa
Jean Carlos <jcpds14@gmail.com>
2022-09-22 11:00:52 -0300

    add Curso Class and List Pessoa, learning about void, list type, method

---
+++ Models/Curso.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }//Lista é uma coleção de um determinado tipo (<Pessoa> neste caso)
+
+        public void AdicionarAluno(Pessoa aluno)//void = vazio, ou seja não retorna nada
+        {
+            Alunos.Add(aluno);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Only studies of C#
