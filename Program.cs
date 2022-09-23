30dc107583a63919c672c028023a40d325dee837
Jean Carlos <jcpds14@gmail.com>
2022-09-23 18:33:58 -0300

    Learning TryParseExact

--- Program.cs
+++ Program.cs
@@ -2,6 +2,74 @@
 using System.Globalization;
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");//Vai alterar todo sistema para esta localizão (Moedas serão formatadas para este local)
 
+string dataString = "2022-04-170";
+
+bool sucesso = DateTime.TryParseExact(
+    dataString, // Pega o nome da variável com a data que você deseja converter
+    "yyyy-MM-dd HH:mm", // aqui deve ser informado como a data esta escrita
+    CultureInfo.InvariantCulture, // Determina a cultura da região.InvarianteCulture para indiferente
+    DateTimeStyles.None, // Estilo da data
+    out DateTime data // formato de saída e nome da para ser aplicado na variável
+    );
+
+if (sucesso)
+{
+    Console.WriteLine($"Conversão realizada com sucesso! Data: {data}");
+}
+else
+{
+    Console.WriteLine($"{dataString} Não é uma data válida.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+DateTime data = DateTime.Now;
+
+Console.WriteLine(data.ToString("dd/MM/yy HH:mm"));
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 decimal valorMonetario = 1582.40M;
 
 Console.WriteLine(valorMonetario.ToString("C2"));
@@ -12,6 +80,7 @@ Console.WriteLine(porcentagem.ToString("P"));
 
 int numero = 123456;
 Console.WriteLine(numero.ToString("##-##-##"));
+*/
 
 
 
