d08a15a456817385584e370c99af09d9a3c09d07
Jean Carlos <jcpds14@gmail.com>
2022-09-24 05:41:36 -0300

    Learning about try/catch and finally methods

--- Program.cs
+++ Program.cs
@@ -2,6 +2,55 @@
 using System.Globalization;
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");//Vai alterar todo sistema para esta localizão (Moedas serão formatadas para este local)
 
+try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine($"\n {linha} \n");
+    }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção. Arquivo não encontrado. {ex.Message}");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção. Caminho da pasta não encontrado. {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
+finally // útil para por exemplo fechar conexão, independente se der erro ou não
+{
+    Console.WriteLine("Passou pelo try catch e chegou no finally");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 string dataString = "2022-04-170";
 
 bool sucesso = DateTime.TryParseExact(
@@ -24,52 +73,14 @@ else
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 DateTime data = DateTime.Now;
 
 Console.WriteLine(data.ToString("dd/MM/yy HH:mm"));
-*/
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 decimal valorMonetario = 1582.40M;
 
 Console.WriteLine(valorMonetario.ToString("C2"));
@@ -80,35 +91,9 @@ Console.WriteLine(porcentagem.ToString("P"));
 
 int numero = 123456;
 Console.WriteLine(numero.ToString("##-##-##"));
-*/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 
 Pessoa p1 = new Pessoa(nome: "Jean", sobrenome: "Carlos");//parametro adicionado pois criei um construtor na classe pessoa. 
 //! Sempre coloque o que cada parametro representa, no caso acima nome: e sobrenome:  isso vai tornar a leitura do código mais fácil e rápida.
